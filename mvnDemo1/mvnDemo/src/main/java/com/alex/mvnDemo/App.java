e8c9dc5d30576b4323692c2ec7686752c07375a8
952121 <50561933+952121@users.noreply.github.com>
2019-05-14 09:40:19 +0800

    Add files via upload

---
+++ mvnDemo1/mvnDemo/src/main/java/com/alex/mvnDemo/App.java
@@ -0,0 +1,15 @@
+package com.alex.mvnDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World 2 " );
+        System.out.println( "Hello World 2 alex" );
+    }
+}
